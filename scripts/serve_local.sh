d47b04f9d072c334ccb87d49f300e84f04c2d5d4
lz-chen <lingzhen.chen@inmeta.no>
2022-08-11 12:41:12 +0200

    add start_up.sh script

--- scripts/serve_local.sh
+++ scripts/serve_local.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 image=$1
+port=$2
 
-docker run -v $(pwd)/test_dir:/opt/ml -p 8989:8080 --rm ${image} serve
+docker run -v $(pwd)/test_dir:/opt/ml -p ${port}:8080 --rm ${image} serve
--- start_up.sh
+++ start_up.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-docker build -t pii-anonymizer ./container
-./scripts/serve_local.sh pii-anonymizer
+
+IMAGE_NAME=${1:-'pii-anonymizer'}
+PORT=${2:-8989}
+docker build -t ${IMAGE_NAME} ./container
+./scripts/serve_local.sh ${IMAGE_NAME} ${PORT}
